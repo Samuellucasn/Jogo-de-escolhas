c4f01ae22f023d15fff7bc78030973580ff90cda
Samuellucasn <samuellucasnogueira@gmail.com>
2022-05-27 21:25:22 -0300

    jogo de escolhas V3

--- mini-jogo.js
+++ mini-jogo.js
@@ -1,3 +1,14 @@
 function finalNovo() {
-    return div.classList.add("final-novo")
+    const div = document.querySelectorAll("#div")
+
+    console.log
+
+    document.addEventListener('click', function(e) {
+
+        const el = e.target
+
+        if (el.classList.contains("final")) {
+            return div.classList.add("final-novo")
+        }
+    })
 }
